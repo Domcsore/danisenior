b3c3d432968180ba7c37cdab9b7aca9638c0b865
Dominic Sore <dominic.sore@optoma.co.uk>
2020-07-01 17:11:18 +0100

    added corejs for polyfill

--- Frontend/src/contactForm.tsx
+++ Frontend/src/contactForm.tsx
@@ -1,23 +1,56 @@
+import 'core-js';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-function ContactForm() {
+type ContactFormProps = {
+    apiEndpoint: string
+}
+
+const ContactForm:React.FunctionComponent<ContactFormProps> = ({apiEndpoint}) => {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    function HandleTextAreaChange(e: React.FormEvent<HTMLTextAreaElement>) {
+        const target = e.target as HTMLTextAreaElement;
+        setMessage(target.value);
+    }
+
+    function HandleInputChange(e: React.FormEvent<HTMLInputElement>) {
+        const target = e.target as HTMLTextAreaElement;
+        switch(target.name) {
+            case "name":
+                setName(target.value)
+                break;
+            case "email":
+                setEmail(target.value)
+                break;
+        }
+    }
+
+    function HandleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+        const params = (new URL(location.href)).searchParams;
+        let ref = params.get("ref") ? params.get("ref") : "home";
+
+    }
+
     return (
-        <form>
-        <div className="inline-two-input">
-            <label>
-                <input type="text" name="name" placeholder="Name"/>
+        <form onSubmit={HandleSubmit}>
+            <div className="inline-two-input">
+                <label>
+                    <input type="text" name="name" placeholder="Name" onChange={HandleInputChange} value={name}/>
+                </label>
+                <label>
+                    <input type="email" name="email" placeholder="Email" onChange={HandleInputChange} value={email}/>
+                </label>
+            </div>
+            <label className="fill">
+                <textarea name="message" placeholder="Message" onChange={HandleTextAreaChange} value={message}></textarea>
             </label>
-            <label>
-                <input type="email" name="email" placeholder="Email"/>
-            </label>
-        </div>
-        <label className="fill">
-            <textarea name="message" placeholder="Message"></textarea>
-        </label>
-        <button className="secondary" type="submit">connect</button>
-    </form>
+            <button className="secondary" type="submit" onSubmit={HandleSubmit}>connect</button>
+        </form>
     )
 }
 
-ReactDOM.render(<ContactForm />, document.getElementById("contact-form-app"));
+ReactDOM.render(<ContactForm apiEndpoint={"faas.dominicsore.com/danisenior/contact"} />, document.getElementById("contact-form-app"));
